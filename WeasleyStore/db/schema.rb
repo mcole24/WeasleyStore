3f307761e0f2e0e30a6bb565eb846970e5e1958e
Matthew Cole <mattaecole@gmail.com>
2017-06-08 19:17:01 +0000

    Set products index page as root route

--- WeasleyStore/db/schema.rb
+++ WeasleyStore/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608174243) do
+ActiveRecord::Schema.define(version: 20170608190812) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "product_name"
+    t.text     "images"
+    t.decimal  "weight"
+    t.integer  "price_cents"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

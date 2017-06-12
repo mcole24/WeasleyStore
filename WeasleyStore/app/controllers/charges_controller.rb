4e1f9a6e3dfc5b7d8759a5e7e8fe27af4227f31d
Matthew Cole <mattaecole@gmail.com>
2017-06-12 22:07:11 +0000

    started get_sum method in charges controller

--- WeasleyStore/app/controllers/charges_controller.rb
+++ WeasleyStore/app/controllers/charges_controller.rb
@@ -1,2 +1,15 @@
 class ChargesController < ApplicationController
+    
+    
+    
+    
+    def get_sum
+       items_in_cart = Items.all
+       sum = 0
+       items_in_cart.each { |i| sum += i }
+       return sum
+    end
+    
+    
+    
 end

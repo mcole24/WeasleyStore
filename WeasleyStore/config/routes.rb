51ce9a013643809925217ed789194e7abaf3ff12
Matthew Cole <mattaecole@gmail.com>
2017-06-12 21:28:33 +0000

    Installed Figaro gem and created Stripe checkout form

--- WeasleyStore/config/routes.rb
+++ WeasleyStore/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'products#index'
   
   resources :products
+  resources :charges
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

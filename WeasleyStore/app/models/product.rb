c90c15082a94ad311f0e91376665a7d25e3f2151
Matthew Cole <mattaecole@gmail.com>
2017-06-13 16:59:33 +0000

    changed price_cents column to galleons

--- WeasleyStore/app/models/product.rb
+++ WeasleyStore/app/models/product.rb
@@ -3,10 +3,10 @@ class Product < ApplicationRecord
     mount_uploaders :images, ImagesUploader
     serialize :images, JSON
     
-    monetize :price_cents, :allow_nil => true,
-    :numericality => {
-    :greater_than_or_equal_to => 0,
-    :less_than_or_equal_to => 10000
-  }
+    #monetize :price_cents, :allow_nil => true,
+    #:numericality => {
+    #:greater_than_or_equal_to => 0,
+    #:less_than_or_equal_to => 10000
+    #}
   
 end
--- WeasleyStore/db/schema.rb
+++ WeasleyStore/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612215500) do
+ActiveRecord::Schema.define(version: 20170613165724) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,9 +29,10 @@ ActiveRecord::Schema.define(version: 20170612215500) do
     t.string   "product_name"
     t.text     "images"
     t.decimal  "weight"
-    t.integer  "price_cents"
+    t.integer  "galleons"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|

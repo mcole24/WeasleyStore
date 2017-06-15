4afe727038f64128d81e0c8813f22d71fb44c9d6
Matthew Cole <mattaecole@gmail.com>
2017-06-15 21:56:59 +0000

    Got functionality for deleting images from product working but only when ALL images for the product are selected.

--- WeasleyStore/app/controllers/products_controller.rb
+++ WeasleyStore/app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:product_name, :images, :weight, :galleons, :description, {images: []})
+      params.require(:product).permit(:product_name, :images, :weight, :galleons, :description, {images: []}, :remove_images)
     end
 end

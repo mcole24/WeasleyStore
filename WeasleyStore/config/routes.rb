b6013d00d356058c18736840626ac408f24abe94
Matthew Cole <mattaecole@gmail.com>
2017-06-16 19:42:55 +0000

    Installed High Voltage gem and created customer list page

--- WeasleyStore/config/routes.rb
+++ WeasleyStore/config/routes.rb
@@ -1,20 +1,29 @@
 Rails.application.routes.draw do
   
-  resources :carts
   devise_for :users
   
   root 'products#index'
   
   resources :products
   resources :charges
+  resources :carts
   
   
   controller :charges do 
     post '/charges/get_sum' => 'charges#get_sum', as: :get_sum
+  end
+  
+  controller :products do
     post '/products/destroy' => 'products#destroy', as: :destroy
   end
   
-  get '/users/index', to: 'users#index'
+  controller :carts do 
+    get '/carts/index' => 'carts#index', as: :customer_list
+  end
+  
+  
+  
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

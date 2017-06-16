6e719f27df6ecce14ecc46263e8d7c5517fa9322
Matthew Cole <mattaecole@gmail.com>
2017-06-16 17:33:52 +0000

    Removed buttons for now until Firefox issue is figured out

--- WeasleyStore/config/routes.rb
+++ WeasleyStore/config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
     post '/products/destroy' => 'products#destroy', as: :destroy
   end
   
+  get '/users/index', to: 'users#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

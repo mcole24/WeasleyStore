5b9802c1756d990b295b94eae128f690fd585341
Matthew Cole <mattaecole@gmail.com>
2017-06-25 19:54:26 +0000

    BUG FIX: Added columns to user table using different local machine

--- WeasleyStore/db/schema.rb
+++ WeasleyStore/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622020852) do
+ActiveRecord::Schema.define(version: 20170625195215) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 20170622020852) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

5091a4959c744d844149419738aa83498c9c33f3
Brandon Lee Upshaw <bupshaw1@my.westga.edu>
2017-07-17 12:21:53 +0000

    Resolved issue with admin update of user profile

--- WeasleyStore/app/controllers/users_controller.rb
+++ WeasleyStore/app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:role, :user_name)
+      params.require(:user).permit(:role, :user_name, :first_name, :last_name, :address, :city, :state)
     end
 
 end
